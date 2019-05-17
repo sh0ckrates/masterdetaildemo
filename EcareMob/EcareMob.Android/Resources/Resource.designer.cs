1490139721c354dfa27e3007e7f55d33a12a3208
pchalaris <pchalaris@cgsoft.gr>
2019-05-17 16:38:47 +0300

    fix

--- EcareMob/EcareMob.Android/Resources/Resource.designer.cs
+++ EcareMob/EcareMob.Android/Resources/Resource.designer.cs
@@ -4448,26 +4448,26 @@ namespace EcareMob.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
+			// aapt resource value: 0x7f02016a
+			public const int avd_hide_password_1 = 2130837866;
+			
 			// aapt resource value: 0x7f02016b
-			public const int avd_hide_password_1 = 2130837867;
+			public const int avd_hide_password_2 = 2130837867;
 			
 			// aapt resource value: 0x7f02016c
-			public const int avd_hide_password_2 = 2130837868;
-			
-			// aapt resource value: 0x7f02016d
-			public const int avd_hide_password_3 = 2130837869;
+			public const int avd_hide_password_3 = 2130837868;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
+			// aapt resource value: 0x7f02016d
+			public const int avd_show_password_1 = 2130837869;
+			
 			// aapt resource value: 0x7f02016e
-			public const int avd_show_password_1 = 2130837870;
+			public const int avd_show_password_2 = 2130837870;
 			
 			// aapt resource value: 0x7f02016f
-			public const int avd_show_password_2 = 2130837871;
-			
-			// aapt resource value: 0x7f020170
-			public const int avd_show_password_3 = 2130837872;
+			public const int avd_show_password_3 = 2130837871;
 			
 			// aapt resource value: 0x7f020056
 			public const int bIoyp5i = 2130837590;
@@ -5237,11 +5237,11 @@ namespace EcareMob.Droid
 			// aapt resource value: 0x7f020155
 			public const int notification_icon_background = 2130837845;
 			
-			// aapt resource value: 0x7f020169
-			public const int notification_template_icon_bg = 2130837865;
+			// aapt resource value: 0x7f020168
+			public const int notification_template_icon_bg = 2130837864;
 			
-			// aapt resource value: 0x7f02016a
-			public const int notification_template_icon_low_bg = 2130837866;
+			// aapt resource value: 0x7f020169
+			public const int notification_template_icon_low_bg = 2130837865;
 			
 			// aapt resource value: 0x7f020156
 			public const int notification_tile_bg = 2130837846;
@@ -5277,28 +5277,25 @@ namespace EcareMob.Droid
 			public const int splash_background = 2130837856;
 			
 			// aapt resource value: 0x7f020161
-			public const int spls_10 = 2130837857;
+			public const int spls_5 = 2130837857;
 			
 			// aapt resource value: 0x7f020162
-			public const int spls_5 = 2130837858;
+			public const int spls_6 = 2130837858;
 			
 			// aapt resource value: 0x7f020163
-			public const int spls_6 = 2130837859;
+			public const int spls_7 = 2130837859;
 			
 			// aapt resource value: 0x7f020164
-			public const int spls_7 = 2130837860;
+			public const int spls_8 = 2130837860;
 			
 			// aapt resource value: 0x7f020165
-			public const int spls_8 = 2130837861;
+			public const int spls_9 = 2130837861;
 			
 			// aapt resource value: 0x7f020166
-			public const int spls_9 = 2130837862;
+			public const int tooltip_frame_dark = 2130837862;
 			
 			// aapt resource value: 0x7f020167
-			public const int tooltip_frame_dark = 2130837863;
-			
-			// aapt resource value: 0x7f020168
-			public const int tooltip_frame_light = 2130837864;
+			public const int tooltip_frame_light = 2130837863;
 			
 			static Drawable()
 			{
